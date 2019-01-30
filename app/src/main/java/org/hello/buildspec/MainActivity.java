b851f4730511bbbb838f0391b2d8b438fa188c87
dongjinkim94 <dongjinkim94@gmail.com>
2019-01-30 20:39:09 +0900

    전체수정
    gradle에 디자인 추가
    main에 java추가
    splash관련 자바, 레이아웃추가
    아이콘 디자인 변경 및 추가

---
+++ app/src/main/java/org/hello/buildspec/MainActivity.java
@@ -0,0 +1,20 @@
+package org.hello.buildspec;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
